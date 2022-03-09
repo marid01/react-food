79c3cb6071ce25e42d5877124dbf145c06e0c8b4
Marid <maridshikhkerimov@gmail.com>
2022-03-09 12:32:37 +0300

    gh-pages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
                     © {new Date().getFullYear()} Copyright Text
                     <a
                         className='grey-text text-lighten-4 right'
-                        href='https://github.com/michey85/react-food'
+                        href='https://github.com/marid01/react-food'
                         rel='noreferrer'
                         target='_blank'
                     >
