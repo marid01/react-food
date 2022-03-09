9404b54cac0a1d7ea892899d14554b6ec8986260
Marid <maridshikhkerimov@gmail.com>
2022-03-09 12:26:06 +0300

    final

---
+++ src/pages/Home.jsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from 'react';
+import { useLocation, useHistory } from 'react-router-dom';
+import { getAllCategories } from '../api';
+import { Preloader } from '../components/Preloader';
+import { CategoryList } from '../components/CategoryList';
+import { Search } from '../components/Search';
+
+function Home() {
+    const [catalog, setCatalog] = useState([]);
+    const [filteredCatalog, setFilteredCatalog] = useState([]);
+
+    const { pathname, search } = useLocation();
+    const hist = useHistory();
+    const { push } = useHistory();
+
+    console.log(hist)
+
+    const handleSearch = (str) => {
+        setFilteredCatalog(
+            catalog.filter((item) =>
+                item.strCategory.toLowerCase().includes(str.toLowerCase())
+            )
+        );
+        push({
+            pathname,
+            search: `?search=${str}`,
+        });
+    };
+
+    console.log(search)
+
+    useEffect(() => {
+        getAllCategories().then((data) => {
+            setCatalog(data.categories);
+            setFilteredCatalog(
+                search
+                    ? data.categories.filter((item) =>
+                          item.strCategory
+                              .toLowerCase()
+                              .includes(search.split('=')[1].toLowerCase())
+                      )
+                    : data.categories
+            );
+        });
+    }, [search]);
+
+    return (
+        <>
+            <Search cb={handleSearch} />
+            {!catalog.length ? (
+                <Preloader />
+            ) : (
+                <CategoryList catalog={filteredCatalog} />
+            )}
+        </>
+    );
+}
+export { Home };
